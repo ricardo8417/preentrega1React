b02ac8bd671ac32477ee64602afc0e74b55e4c1a
Ricardo <rnavarrete_cervantes@hotmail.com>
2023-04-13 19:52:24 -0600

    Segunda-PreEntrega Navarrete

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,19 @@
 import './App.css';
+
+//Componentes
 import {Menu} from './Navbar/NavBar'
-import ItemListContainer from './ItemListContainer/ItemListContainer';
+
+import { ItemListContainer } from './ItemListContainer/ItemListContainer';
+import { ItemDetailContainer } from './ItemDetailContainer/ItemDetailContainer';
 function App() {
   return (
-    <div className="App">
+    <>
     <Menu />
-    <ItemListContainer greeting={'Lista de Productos'} />
-    </div>
+     
+      <ItemListContainer />
+      <ItemDetailContainer />
+
+    </>
   );
 }
 
--- src/Components/CartWidget/CartWidget.jsx
+++ src/Components/CartWidget/CartWidget.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+
 import cart from './assets/carrito.svg'
 export const CartWidget= ({cantCart}) => {
   return (
-    <div className='container-cart'>
+    <>
       <img src= {cart} alt="cart-widget" />
      <p>{cantCart}</p>
-          </div>
+          </>
           
   )
 }
